ebd8a32fa248f778eacc76ff661cc3fa39900b48
C-Nasir <Nasrulahi90@gmail.com>
2019-05-25 14:48:25 -0500

    Xamarin Behavior
    This is how to use xamarin behavior

---
+++ XamarinFormBehavior/XamarinFormBehavior/Behaviors/BackgroundColorChangeBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+namespace XamarinFormBehavior
+{
+    public class BackgroundColorChangeBehavior : Behavior<Picker>
+    {
+        protected override void OnAttachedTo(Picker bindable)
+        {
+            bindable.SelectedIndexChanged += OnItemChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Picker bindable)
+        {
+            bindable.SelectedIndexChanged -= OnItemChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnItemChanged(object sender, EventArgs args)
+        {
+            var item = sender as Picker;
+
+            switch (item.SelectedIndex)
+            {
+                case 1:
+                    item.BackgroundColor = Color.Cyan;
+                    break;
+                case 2:
+                    item.BackgroundColor = Color.Yellow;
+                    break;
+                case 3:
+                    item.BackgroundColor = Color.Green;
+                    break;
+                case 4:
+                    item.BackgroundColor = Color.Brown;
+                    break;
+                default:
+                    item.BackgroundColor = Color.Default;
+                    break;
+            }
+        }
+    }
+}
---
+++ XamarinFormBehavior/XamarinFormBehavior/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormBehavior
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public string[] colors = { "Pick a Color","Cyan", "Yellow", "Green", "Brown" };
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ColorPicker.ItemsSource = colors;
+            ColorPicker.SelectedIndex = 0;
+        }
+    }
+}
