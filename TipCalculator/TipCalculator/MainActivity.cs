fb54c04931c131dffaf388ebb10d5b583eae39b1
C-Nasir <Nasrulahi90@gmail.com>
2019-05-26 16:22:33 -0500

    Xamarin.Andoid Project
    This is a program that calculates the tip of a meal or service when the user enters an amount. I built it with Xamarin.Android

---
+++ TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace TipCalculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            //setting the views nams
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClicked;
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+
+
+        private void OnCalculateClicked(object sender, EventArgs args)
+        {
+            double amount;
+
+            if(double.TryParse(inputBill.Text, out amount))
+            {
+                var tip = .15 * amount;
+                var total = amount + tip;
+
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString();
+            }
+        }
+
+    }
+}
+
